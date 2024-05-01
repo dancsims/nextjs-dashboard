688d18e81fbebe1883c48111b622bb23d8b0e6f7
Dan <sims.daniel.ds@icloud.com>
2024-04-30 21:18:52 -0400

    up through chapter 11

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,10 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div
-  className={styles.shape}/>
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div className={styles.shape} />
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -37,14 +38,14 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
-            />
-            <Image
+          />
+          <Image
             src="/hero-mobile.png"
             alt="Screenshots of the dashboard project showing mobile version"
             width={560}
             height={620}
             className="block md:hidden"
-            />
+          />
         </div>
       </div>
     </main>
