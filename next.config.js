e5262f33fe9464b9c83e2159e5afb39cdd99e46e
Dan <sims.daniel.ds@icloud.com>
2024-05-01 09:52:49 -0400

    Chapter 12

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '192.168.1.56:3000'],
+    },
+  },
+};
 
 module.exports = nextConfig;
